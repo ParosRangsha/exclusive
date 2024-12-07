aaeec5e7d5d07ef47119d44d117fbb5fd551bfc6
paros <paroscrangsha@gmail.com>
2024-12-07 12:41:51 +0600

    timer add

--- src/assets/pages/home/Home.jsx
+++ src/assets/pages/home/Home.jsx
@@ -1,8 +1,66 @@
 import React from 'react'
+import './home.css'
+import { Col, Container, Row } from 'react-bootstrap'
+import { Link } from 'react-router-dom'
+import homeban from '../../../img/homebanner.jpg'
+import './count.js'
 
 const Home = () => {
   return (
-    <div>Home</div>
+    <div className="home">
+      <div className="banner">
+        <Container>
+          <Row>
+            <Col lg={3} className='itemList'>
+              <ul>
+                <li><Link to="/">Woman’s Fashion</Link></li>
+                <li><Link to="/">Men’s Fashion</Link></li>
+                <li><Link to="/">Electronics</Link></li>
+                <li><Link to="/">Home & Lifestyle</Link></li>
+                <li><Link to="/">Medicine</Link></li>
+                <li><Link to="/">Sports & Outdoor</Link></li>
+                <li><Link to="/">Baby’s & Toys</Link></li>
+                <li><Link to="/">Groceries & Pets</Link></li>
+                <li><Link to="/">Health & Beauty</Link></li>
+              </ul>
+            </Col>
+            <Col lg={9} className='homebanner'>
+              <img src={homeban} alt="home banner" />
+            </Col>
+          </Row>
+        </Container>
+      </div>
+      <div className="todays">
+        <Container>
+          <Row className='subhead'>
+            <h5>Today's Collections</h5>
+          </Row>
+          <Row>
+            <Col lg={3}>
+              <h2>Flash Sales</h2>
+            </Col>
+            <Col lg={3} className='timer'>
+              <div className="days">
+                <h4>Days</h4>
+                <h3 id='days'></h3>
+              </div>
+              <div className="Hours">
+                <h4>Hours</h4>
+                <h3 id='hours'></h3>
+              </div>
+              <div className="days">
+                <h4>Minutes</h4>
+                <h3 id='minutes'></h3>
+              </div>
+              <div className="seconds">
+                <h4>Seconds</h4>
+                <h3 id='seconds'></h3>
+              </div>
+            </Col>
+          </Row>
+        </Container>
+      </div>
+    </div>
   )
 }
 
---
+++ src/assets/pages/home/count.js
@@ -0,0 +1,19 @@
+let countDownDate = new Date("Jan 5, 2025 12:00:00").getTime();
+let x = setInterval(function() {
+let now = new Date().getTime();
+
+let distance = countDownDate - now;
+let days = Math.floor(distance / (10000 * 60 * 60 * 60));
+let hours = Math.floor((distance % (1000 * 60 * 60 * 60)) / (1000 * 60 * 60));
+let minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+let seconds = Math.floor((distance % (1000 * 60)) / 1000);
+
+  document.getElementById("days").innerHTML = days + "d ";
+  document.getElementById("hours").innerHTML = hours + "h ";
+  document.getElementById("minutes").innerHTML = minutes + "m ";
+  document.getElementById("seconds").innerHTML = seconds + "m ";
+  if (distance < 0) {
+    clearInterval(x);
+    document.getElementById("days").innerHTML = "EXPIRED";
+  }
+}, 1000);
