8d04473f33c235a54114ed8e2c533406b27ee3c2
Paros Rangsha <paroscrangsha@gmail.com>
2024-12-07 15:17:15 +0000

    footer full update

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -1,6 +1,8 @@
 import React from 'react'
 import './footer.css'
 import { Col, Container, Row } from 'react-bootstrap'
+import { Link } from 'react-router-dom'
+import playstore from '../../img/playstore.png'
 
 const Footer = () => {
   return (
@@ -20,12 +22,63 @@ const Footer = () => {
               </li>
             </ul>
           </Col>
-          <Col lg={3} className='Support'>
+          <Col lg={2} className='Support'>
             <ul>
               <li><h4>Support</h4></li>
               <li>111 Bijoy sarani, Dhaka,  DH 1515, Bangladesh.</li>
               <li>exclusive@gmail.com</li>
               <li>+88017-1234-5678</li>
+            </ul>
+          </Col>
+          <Col lg={2} className='account'>
+            <ul>
+              <li><h4>My Account</h4></li>
+              <li>
+                <Link to="/account">My Account</Link>
+              </li>
+              <li>
+                <Link to="/login">Login</Link>/
+                <Link to="/register">Register</Link>
+              </li>
+              <li>
+                <Link to="/cart">Cart</Link>
+              </li>
+              <li>
+                <Link to="/wishlist">Wishlist</Link>
+              </li>
+              <li>
+                <Link to="/shop">Shop</Link>
+              </li>
+            </ul>
+          </Col>
+          <Col lg={2} className='quicklink'>
+            <ul>
+              <li><h4>Quick Link</h4></li>
+              <li>
+                <Link to="/privacypolicy">Privacy Policy</Link>
+              </li>
+              <li>
+                <Link to="/termsofuse">Terms Of Use</Link>
+              </li>
+              <li>
+                <Link to="/faq">FAQ</Link>
+              </li>
+              <li>
+                <Link to="/contact">Contact</Link>
+              </li>
+            </ul>
+          </Col>
+          <Col lg={3}>
+            <ul>
+              <li><h4>Download App</h4></li>
+              <li><p>Save $3 with App New User Only</p></li>
+              <li><img src={playstore} alt="playstore"/></li>
+              <li className='socialicon'>
+                <Link to="/"><i class="fa-brands fa-facebook-f"></i></Link>
+                <Link to="/"><i class="fa-brands fa-twitter"></i></Link>
+                <Link to="/"><i class="fa-brands fa-instagram"></i></Link>
+                <Link to="/"><i class="fa-brands fa-linkedin-in"></i></Link>
+              </li>
             </ul>
           </Col>
         </Row>
