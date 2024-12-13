a7f4361b168cb36410a14f6316e1979f796d04e8
paros <paroscrangsha@gmail.com>
2024-12-13 08:30:12 +0600

    signup add

--- src/assets/pages/signup/Signup.jsx
+++ src/assets/pages/signup/Signup.jsx
@@ -1,8 +1,30 @@
 import React from 'react'
+import { Col, Row } from 'react-bootstrap'
+import './signup.css'
+import signupthumb from '../../../img/signupthumb.png'
+import { Link } from 'react-router-dom'
 
 const Signup = () => {
   return (
-    <div>Signup</div>
+    <div className="signup">
+      <Row>
+        <Col lg={8}>
+          <img src={signupthumb} alt="" />
+        </Col>
+        <Col lg={4}>
+          <div className="box">
+            <h3>Create an account</h3>
+            <p>Enter your details below</p>
+            <input type="text" placeholder='Your name' />
+            <input type="text" placeholder='Your e-mail or phone number' />
+            <input type="password" placeholder='Password' />
+            <Link to='/createaccount' id='create'>Create Account</Link>
+            <Link to='/withgoogle' id='logwithgoogle'><i className="fa-brands fa-google"></i> Sign up with Google</Link>
+            <p className='alreadyaccount'>Already have account? <Link to='/login'>Log In</Link></p>
+          </div>
+        </Col>
+      </Row>
+    </div>
   )
 }
 
